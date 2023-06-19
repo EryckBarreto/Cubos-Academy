e156cdc4523841a9cff3c521f4ec12ac56c3510e
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-19 15:38:58 -0300

    uso de string template

--- modulo1/variaveis/variaveis-number.js
+++ modulo1/variaveis/variaveis-number.js
@@ -4,3 +4,7 @@ console.log(idade);
 //camelCase
 let alturaEmMetros = 1.71;
 console.log(alturaEmMetros);
+
+//string template
+
+console.log(`Minha idade é ${idade} anos e minha altura é ${alturaEmMetros} metros.`);
