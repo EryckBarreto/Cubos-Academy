9a408f3786f8947998cbc4a5d2c3e19a947703cf
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-19 15:32:00 -0300

    diferença do uso do let e do const

--- modulo1/variaveis/variaveis-const.js
+++ modulo1/variaveis/variaveis-const.js
@@ -1,3 +1,10 @@
 const primeiroNome = "Eryck";
 
 console.log(primeiroNome);
+
+// const não reatribui valor, let sim
+
+let segundoNome = "Barreto";
+segundoNome = "Silva";
+
+console.log(segundoNome);
