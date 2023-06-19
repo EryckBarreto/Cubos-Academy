25a005490203d8b3f714b79cd33f757900a9f8c3
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-19 16:10:36 -0300

    exemplificando null e undefined

---
+++ modulo1/variaveis/variaveis-tipos.js
@@ -0,0 +1,8 @@
+let nome = "Eryck"; //string
+let idade = 30; //number
+let altura = 1.71; //number
+
+let nota;
+console.log(nota); //undefined
+nota = null; //indica valor nulo
+console.log(nota); // null
