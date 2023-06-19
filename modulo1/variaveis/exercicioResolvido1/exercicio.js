b862cf252e1a5c354ceaf38c1fcb9f90310f4c0d
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-19 15:50:27 -0300

    resolução de exercícios propostos em aula

---
+++ modulo1/variaveis/exercicioResolvido1/exercicio.js
@@ -0,0 +1,2 @@
+const cidadeNatal = "Curitiba";
+console.log(cidadeNatal);
---
+++ modulo1/variaveis/exercicioResolvido1/exercicio2.js
@@ -0,0 +1,5 @@
+let populacao;
+populacao = 250.000;
+console.log(populacao);
+populacao = 350.000;
+console.log(populacao);
