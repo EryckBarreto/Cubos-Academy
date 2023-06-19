632648b581447db5c6409ed390484267195ad4a0
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-19 16:41:45 -0300

    realização de exercícios propostos em aula sobre op aritméticas

---
+++ modulo1/variaveis/exercicioResolvido1/exercicio3.js
@@ -0,0 +1,17 @@
+let x = 10, y = 2;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}.`);
+
+resultado = x - y;
+
+console.log(`A subtração de ${x} e ${y} é ${resultado}.`);
+
+resultado = x * y;
+console.log(`A multiplicação de ${x} e ${y} é ${resultado}.`);
+
+resultado = x / y;
+console.log(`A divisão de ${x} e ${y} é ${resultado}.`);
+
+resultado = x % y;
+console.log(`O resto da divisão de ${x} e ${y} é ${resultado}.`);
---
+++ modulo1/variaveis/variaveis-concatenacao.js
@@ -0,0 +1,5 @@
+let nome = "Eryck";
+let sobrenome = "Barreto";
+
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
