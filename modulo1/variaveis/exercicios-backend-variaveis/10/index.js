2a2d6633df3a396c564c2eef243013ccb5c24408
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-20 11:29:12 -0300

    Resolução de exercício para encontrar delta em bhaskara

---
+++ modulo1/variaveis/exercicios-backend-variaveis/10/index.js
@@ -0,0 +1,6 @@
+function valorDeltaEmBhaskara(b, a, c) {
+    const delta = Math.pow(b, 2) - (4 * a * c);
+    return console.log(`O valor de delta é de ${delta}.`)
+}
+
+valorDeltaEmBhaskara(5, 5, 1);
