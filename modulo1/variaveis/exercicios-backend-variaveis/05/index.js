08cbb3eb4a4f0cf98a098982405c8cdcd9091af3
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-19 21:35:44 -0300

    Resolução do exercício de cálculo entre 2 pontos.

---
+++ modulo1/variaveis/exercicios-backend-variaveis/05/index.js
@@ -0,0 +1,6 @@
+function distanciaDoisPontos(x1, x2, y1, y2) {
+    resultado = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+    return console.log(`A distância entre os dois pontos é de ${resultado}.`)
+}
+
+distanciaDoisPontos(1, 1, 1, 4);
