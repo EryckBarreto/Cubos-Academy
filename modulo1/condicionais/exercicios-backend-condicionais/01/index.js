3ea68e3d56b45928dfad8e92466a26cf4bf8d908
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 15:42:12 -0300

    Resolução do exercício  de pedra, papel e tesoura

--- modulo1/condicionais/exercicios-backend-condicionais/01/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/01/index.js
@@ -1,4 +1,11 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "pedra";
+const jogada2 = "papel";
 
-//seu código aqui .
+
+if ((jogada1 === "pedra" && jogada2 === "tesoura") || (jogada1 === "tesoura" && jogada2 === "papel") || (jogada1 === "papel" && jogada2 === "pedra")) {
+    console.log(`Jogada vencedora: ${jogada1.toUpperCase()}!`);
+    } else if ((jogada2 === "pedra" && jogada1 === "tesoura") || (jogada2 === "tesoura" && jogada1 === "papel") || (jogada2 === "papel" && jogada1 === "pedra")) {
+        console.log(`Jogada vencedora: ${jogada2.toUpperCase()}!`);
+    } else if (jogada1 === jogada2) {
+        console.log("EMPATE");
+    }
