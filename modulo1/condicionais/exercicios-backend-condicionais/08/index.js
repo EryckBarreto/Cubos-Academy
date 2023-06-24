81373a369d41b848ad23d89992350c59b226d024
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 17:02:56 -0300

    Resolução do exercício "Montanha Russa Muito Assustadora"

--- modulo1/condicionais/exercicios-backend-condicionais/08/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/08/index.js
@@ -1,5 +1,12 @@
-const idade = 18;
+const idade = 11;
 const possuiPatologia = false;
 const altura = 180;
-const ehEstudante = false;
+const ehEstudante = true;
 
+if ((idade < 12 || idade > 65) || possuiPatologia || altura < 150) {
+    console.log("ACESSO NEGADO");
+} else if (ehEstudante) {
+    console.log("Você é estudante! E aqui estudante paga meia, passa os R$ 10,00.");
+} else {
+    console.log("Valor da entrada é de R$ 20,00.");
+}
