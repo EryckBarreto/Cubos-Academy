b4cedba69395bd93778ae5cedcf6312c05113ba7
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 16:40:57 -0300

    resolução do exercicios "definindo as posições"

--- modulo1/condicionais/exercicios-backend-condicionais/06/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,15 @@
-const alturaEmCm = 185;
+const alturaEmCm = 197;
 
 //seu código aqui
+
+if (alturaEmCm < 180) {
+    console.log("REPROVADO");
+} else if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+    console.log("Posição adequada: LÍBERO");
+} else if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+    console.log("Posição adequada: PONTEIRO");
+} else if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+    console.log("Posição adequada: OPOSTO");
+} else {
+    console.log("Posição adequada: CENTRAL");
+} 
