b2147b6cc1f6c750c432c4705344e50a16086812
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 17:13:01 -0300

    resolução do exercício "transformar nota em conceito"

--- modulo1/condicionais/exercicios-backend-condicionais/09/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,16 @@
-const nota = 8.5;
+const nota = 8.7;
+
+
+if (nota > 10 || nota < 0) {
+    console.log("Nota inválida, verifique o valor digitado, deve ser de 0 à 10.");
+} else if (nota >= 9) {
+    console.log(`A nota do aluno foi de ${nota}, portanto obteve conceito A.`);
+} else if (nota >= 8 && nota <= 8.9) {
+    console.log(`A nota do aluno foi de ${nota}, portanto obteve conceito B.`);
+} else if (nota >= 6 && nota <= 7.9) {
+    console.log(`A nota do aluno foi de ${nota}, portanto obteve conceito C.`);
+} else if (nota >= 4 && nota <= 5.9) {
+    console.log(`A nota do aluno foi de ${nota}, portanto obteve conceito D.`);
+} else {
+    console.log(`A nota do aluno foi de ${nota}, portanto obteve conceito E.`);
+}
