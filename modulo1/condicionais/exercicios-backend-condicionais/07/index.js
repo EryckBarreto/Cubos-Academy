50a4c2f9fcfbf07f70993c6f9435b6d448a84683
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 16:51:16 -0300

    resolução do exercício de imposto de renda

--- modulo1/condicionais/exercicios-backend-condicionais/07/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/07/index.js
@@ -3,3 +3,11 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+
+if (aposentada || portadoraDeDoenca) {
+    console.log("Pessoa isenta do pagamento de Imposto de Renda.");
+} else if (totalDeRendimentos < 2855970) {
+    console.log("SAI DAQUI LEÃO, RUIM SEM VOCÊ, PIOR CONTIGO!");
+} else {
+    console.log("PEGA LEÃO!");
+}
