58ebde7d61f4674707355c1e7340ad2fd6b29ded
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 15:59:51 -0300

    Resolução do exercício dominó

--- modulo1/condicionais/exercicios-backend-condicionais/03/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/03/index.js
@@ -1,4 +1,10 @@
-const ladoA = 3;
-const ladoB = 3;
+const ladoA = 6;
+const ladoB = 6;
 
 //seu código aqui
+
+if (ladoA === ladoB) {
+    console.log("Sim, é uma BUCHA!");
+} else {
+    console.log("Não é BUCHA!");
+}
