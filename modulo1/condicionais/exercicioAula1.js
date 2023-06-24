754ad07e924c9fc323f8f6e4429dc606effd81e3
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 12:08:05 -0300

    Corrigida mensagem ao cliente

--- modulo1/condicionais/exercicioAula1.js
+++ modulo1/condicionais/exercicioAula1.js
@@ -3,4 +3,4 @@ const numeroDeParcelas = 2;
 const valorFinal = numeroDeParcelas === 1 ? valorDaCompra * 0.90 : valorDaCompra / numeroDeParcelas;
 const parcelaOuParcelas = numeroDeParcelas === 1 ? "parcela à vista" : "parcelas";
 
-console.log(`Você pode pagar à vista com 10% de desconto ou parcelado em até 6x sem juros. Como você escolheu ${numeroDeParcelas} ${parcelaOuParcelas}, o valor final é de ${valorFinal.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} reais.`);
+console.log(`Você pode pagar à vista com 10% de desconto ou parcelado em até 6x sem juros. Como você escolheu ${numeroDeParcelas} ${parcelaOuParcelas}, o valor é de ${valorFinal.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} reais.`);
