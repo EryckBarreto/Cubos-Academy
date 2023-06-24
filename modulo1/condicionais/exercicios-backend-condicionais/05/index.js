f012fee1ec3b4da99c3245e6f400d0856c6e6b9d
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 16:34:05 -0300

    Resolução do exercício peneira de volei

--- modulo1/condicionais/exercicios-backend-condicionais/05/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/05/index.js
@@ -1,3 +1,9 @@
-const alturaEmCm = 185;
+const alturaEmCm = 15;
 
 //seu código aqui
+
+if (alturaEmCm >= 180) {
+    console.log("JOGADOR APROVADO");
+} else {
+    console.log("JOGADOR REPROVADO");
+}
