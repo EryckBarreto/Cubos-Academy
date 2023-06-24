7e5fd6975d2e47468badecbdd23269d14e8e6e03
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-24 15:48:08 -0300

    Código refatorado

--- modulo1/condicionais/exercicios-backend-condicionais/01/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/01/index.js
@@ -1,11 +1,19 @@
-const jogada1 = "pedra";
-const jogada2 = "papel";
+const jogada1 = "tesoura";
+const jogada2 = "tesoura";
 
 
-if ((jogada1 === "pedra" && jogada2 === "tesoura") || (jogada1 === "tesoura" && jogada2 === "papel") || (jogada1 === "papel" && jogada2 === "pedra")) {
-    console.log(`Jogada vencedora: ${jogada1.toUpperCase()}!`);
-    } else if ((jogada2 === "pedra" && jogada1 === "tesoura") || (jogada2 === "tesoura" && jogada1 === "papel") || (jogada2 === "papel" && jogada1 === "pedra")) {
-        console.log(`Jogada vencedora: ${jogada2.toUpperCase()}!`);
-    } else if (jogada1 === jogada2) {
-        console.log("EMPATE");
-    }
+// if ((jogada1 === "pedra" && jogada2 === "tesoura") || (jogada1 === "tesoura" && jogada2 === "papel") || (jogada1 === "papel" && jogada2 === "pedra")) {
+//     console.log(`Jogada vencedora: ${jogada1.toUpperCase()}!`);
+//     } else if ((jogada2 === "pedra" && jogada1 === "tesoura") || (jogada2 === "tesoura" && jogada1 === "papel") || (jogada2 === "papel" && jogada1 === "pedra")) {
+//         console.log(`Jogada vencedora: ${jogada2.toUpperCase()}!`);
+//     } else if (jogada1 === jogada2) {
+//         console.log("EMPATE");
+// }
+
+if (jogada1 === jogada2) {
+    console.log("EMPATE");
+} else if ((jogada1 === "pedra" && jogada2 === "tesoura") || (jogada1 === "tesoura" && jogada2 === "papel") || (jogada1 === "papel" && jogada2 === "pedra")) {
+    console.log(`Jogada vencedora: ${jogada1.toUpperCase()}!`); 
+} else {
+    console.log(`Jogada vencedora: ${jogada2.toUpperCase()}!`);
+}
