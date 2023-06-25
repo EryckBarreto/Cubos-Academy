a61dcdb0f902dbaf0708e522989ee8388ebd7e80
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 18:02:49 -0300

    Aula sobre while em JavaScript

---
+++ modulo1/arraysLoops/while.js
@@ -0,0 +1,8 @@
+const nomeDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+
+let indice = 0;
+
+while (indice < nomeDeEstudantes.length) {
+    console.log(nomeDeEstudantes[indice]);
+    indice++;
+}
