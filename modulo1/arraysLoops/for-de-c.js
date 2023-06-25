8ccdb8662aeb1e5ffdd1cef5da4268e2307b51e9
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 18:13:38 -0300

    Uso do for classico em JavaScript

---
+++ modulo1/arraysLoops/for-de-c.js
@@ -0,0 +1,12 @@
+const nomeDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+
+// let indice = 0;
+
+// while (indice < nomeDeEstudantes.length) {
+//     console.log(nomeDeEstudantes[indice]);
+//     indice++;
+// }
+
+for (let i = 0; i < nomeDeEstudantes.length; i++) {
+    console.log(nomeDeEstudantes[i]);
+}
