ec11e0e498ce89ea7e35a105fd26e9ade653834f
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 20:29:11 -0300

    resolução do exercício "filtrando nome com a letra A"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/07/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/07/index.js
@@ -1,1 +1,10 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomes = ["Ana", "Joana", "Carlos", "amanda", "Andressa", "antonia"];
+const nomeComInicioA = [];
+
+for (let nome of nomes) {
+    if (nome.toLocaleLowerCase()[0] === "a") {
+        nomeComInicioA.push(nome);
+    }
+}
+
+console.log(nomeComInicioA);
