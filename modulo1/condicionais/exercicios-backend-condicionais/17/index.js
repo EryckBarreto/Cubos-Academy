d8ae57441a6e0157c74bd2f3f582707eb754988e
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 17:20:00 -0300

    atualização do código

--- modulo1/condicionais/exercicios-backend-condicionais/17/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/17/index.js
@@ -6,5 +6,10 @@ const valorRestante = valorDoProduto - valorPago;
 const parcelasPagas = valorPago * quantidadeDoParcelamento / (valorDoProduto);
 const parcelasRestantes = quantidadeDoParcelamento - parcelasPagas;
 
+// considerando a proposta do exercicio, o valor das 3 primeiras constantes precisam
+// ser coerentes. Se foi realizado um valor total de 2000 mil em 10x, o valor pago não
+// poder ser 150 por exemplo pois o exercício não deixa claro se o valor pago é a título de entrada
+// pelo contrário, deixa a entender que é em relação a  quantidade de parcelas pagas.
+
 console.log(`Você parcelou o produto de R$ ${(valorDoProduto).toFixed(2)} em ${quantidadeDoParcelamento}x, pagou ${parcelasPagas} parcelas, no valor total de R$ ${valorPago.toFixed(2)}.`)
 console.log(`Faltam ${parcelasRestantes} parcelas, totalizando o valor de R$ ${valorRestante.toFixed(2)}.`);
