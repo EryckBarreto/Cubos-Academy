e24d563c9339a179da850bca49b2b739189091eb
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 18:17:24 -0300

    Resolução do exercicio 1 proposto em aula

---
+++ modulo1/arraysLoops/exercicio1.js
@@ -0,0 +1,12 @@
+const variosNumeros = [1, 3, 5, 7, 9, 10];
+
+// let i = 0;
+
+// while (i < variosNumeros.length) {
+//     console.log(variosNumeros[i]);
+//     i++;
+// }
+
+for (let i = 0; i < variosNumeros.length; i++) {
+    console.log(variosNumeros[i]);
+}
