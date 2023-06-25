ba730f3fbf188c7b793535a28e17245f871fc85f
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 11:54:16 -0300

    resolução do exercício compra com desconto

--- modulo1/condicionais/exercicios-backend-condicionais/13/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/13/index.js
@@ -1,6 +1,15 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+const tipoDePagamento = "cheque";
 
 //valor da mercadoria (centavos)
-const valorDoProduto = 13000;
+const valorDoProduto = 18000;
 
+if (tipoDePagamento === "dinheiro" || tipoDePagamento === "debito") {
+    console.log(`A modalidade de pagamento escolhida tem 10% de desconto, valor final do produto é de R$ ${(valorDoProduto/100 * 0.90).toFixed(2)}.`);
+} else if (tipoDePagamento === "credito") {
+    console.log(`A modalidade de pagamento escolhida tem 5% de desconto, valor final do produto é de R$ ${(valorDoProduto/100 * 0.95).toFixed(2)}.`);
+} else if (tipoDePagamento === "cheque"){
+    console.log(`A modalidade de pagamento escolhida tem 3% de desconto, valor final do produto é de R$ ${(valorDoProduto/100 * 0.97).toFixed(2)}.`);
+} else {
+    console.log("Modalidade de pagamento inválida, as opções são: dinheiro, credito, debito ou cheque.");
+}
