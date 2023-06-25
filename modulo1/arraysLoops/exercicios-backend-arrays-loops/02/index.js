20478ae6107bc82677119e5aec767b5f46cfdaad
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 19:41:00 -0300

    Resolução do exercício "contando letras"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/02/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/02/index.js
@@ -1,1 +1,18 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+
+let soma = 0;
+let letraEncontrada = false
+
+
+for (let letra of letras) {
+    if (letra.toLocaleLowerCase() === "e") {
+        soma++;
+        letraEncontrada = true;
+    }
+}
+
+if (!letraEncontrada) {
+    console.log('Nenhuma letra "E" ou "e" encontrada :( ');
+} else {
+    console.log(`Foram encontradas ${soma} letras "E" ou "e".`);
+}
