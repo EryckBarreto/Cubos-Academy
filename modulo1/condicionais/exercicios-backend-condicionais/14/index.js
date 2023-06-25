bdb73e576820a759e8b2c8069252d1676219bd1a
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 12:01:59 -0300

    resolução do exercício de consumo de agua ingerida

--- modulo1/condicionais/exercicios-backend-condicionais/14/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/14/index.js
@@ -1,3 +1,12 @@
 //quantidade de água ingerida em litro.
-const quantidadeDeAguaIngerida = 2;
+const quantidadeDeAguaIngerida = "a";
 
+if (quantidadeDeAguaIngerida >= 0 && quantidadeDeAguaIngerida < 1.5) {
+    console.log("Risco Alto - Você está ingerindo pouquissima água, beba mais água!");
+} else if (quantidadeDeAguaIngerida >= 1.5 && quantidadeDeAguaIngerida <= 3) {
+    console.log("Risco Moderado - Você está ingerindo pouca água, beba mais!");
+} else if (quantidadeDeAguaIngerida > 3) {
+    console.log("Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!");
+} else {
+    console.log("Valor inválido, insira um número maior que 0.");
+}
