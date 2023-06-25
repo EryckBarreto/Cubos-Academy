919b52605df43be419cea98a5359d836e7e58803
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 12:45:50 -0300

    resolução do exercício rematrícula escolar

--- modulo1/condicionais/exercicios-backend-condicionais/16/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/16/index.js
@@ -1,5 +1,14 @@
 //Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
+const idadeDoAluno = 14;
 
 //valor verdadeiro ou falso que informa se possui responsável
-const possuiResponsavel = false;
+const possuiResponsavel = true;
+
+// if (idadeDoAluno >= 18 || (idadeDoAluno < 18 && possuiResponsavel)) {
+//     console.log("Matrícula realizada com sucesso!");
+// } else {
+//     console.log("Não é possível realizar a matrícula.");
+// }
+
+const podeFazerMatricula = (idadeDoAluno >= 18 || (idadeDoAluno < 18 && possuiResponsavel)) ? "Matricula realizada com sucesso." : "Não é possível realizar a matrícula."
+console.log(podeFazerMatricula);
