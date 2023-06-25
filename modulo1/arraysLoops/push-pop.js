c25b4a1d7892aa3bb698fbcd0c37cbd39059193f
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 17:41:06 -0300

    Uso do push e do pop em JavaScript

---
+++ modulo1/arraysLoops/push-pop.js
@@ -0,0 +1,10 @@
+const nomeDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+
+nomeDeEstudantes.push("aninha");
+
+console.log(nomeDeEstudantes);
+
+// o pop tira o último item do array
+nomeDeEstudantes.pop();
+
+console.log(nomeDeEstudantes);
