d2bb784a07e7bc6f8dfda648e664de4729828e72
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 13:10:03 -0300

    resolução do exercício extrato de compra online

--- modulo1/condicionais/exercicios-backend-condicionais/17/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/17/index.js
@@ -1,8 +1,10 @@
 //valor do produto comprado.
-const valorDoProduto = 100000;
-
-//quantidade de parcelas
+const valorDoProduto = 200000/100;
 const quantidadeDoParcelamento = 10;
+const valorPago = 600;
+const valorRestante = valorDoProduto - valorPago;
+const parcelasPagas = valorPago * quantidadeDoParcelamento / (valorDoProduto);
+const parcelasRestantes = quantidadeDoParcelamento - parcelasPagas;
 
-//valor pago
-const valorPago = 300;
+console.log(`Você parcelou o produto de R$ ${(valorDoProduto).toFixed(2)} em ${quantidadeDoParcelamento}x, pagou ${parcelasPagas} parcelas, no valor total de R$ ${valorPago.toFixed(2)}.`)
+console.log(`Faltam ${parcelasRestantes} parcelas, totalizando o valor de R$ ${valorRestante.toFixed(2)}.`);
