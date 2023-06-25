937f14e7ea24e58946815e6994e2e931a4e84b61
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 20:12:03 -0300

    resolução do exercício "filtrando apenas os pares"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/05/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/05/index.js
@@ -1,1 +1,10 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 12, 53, 88, 112, 122, 124, 126];
+const numerosPares = [];
+
+for (let numero of original) {
+    if (numero % 2 === 0) {
+        numerosPares.push(numero);
+    }
+}
+
+console.log(numerosPares);
