56d37b657db388a938590001d1b3a7f6a5ed6670
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 19:59:40 -0300

    resolucao do exercicio "encontre o 10"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/03/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/03/index.js
@@ -1,2 +1,12 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+let posicao = -1;
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] === 10) {
+        posicao = i;
+    }
+}
+
+console.log(posicao);
+
