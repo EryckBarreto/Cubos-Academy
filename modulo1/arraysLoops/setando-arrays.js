f554c1ef3135b9229374d41e74f2a0bd0d65cd99
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 17:30:21 -0300

    aula sobre como alterar arrays

---
+++ modulo1/arraysLoops/setando-arrays.js
@@ -0,0 +1,10 @@
+const nomeDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+
+// trocar item de um array
+// não está modificando a variável, e sim o conteúdo do array
+
+const x = 3;
+
+nomeDeEstudantes[0] = "andreia";
+nomeDeEstudantes[x] = "epaminondas"
+console.log(nomeDeEstudantes);
