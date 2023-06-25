e8ef7f5cf8fe20df0ff01a208549543cd7ef9ed8
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 11:35:53 -0300

    resolução do exercício dias da semana

--- modulo1/condicionais/exercicios-backend-condicionais/12/index.js
+++ modulo1/condicionais/exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,26 @@
-const diaDaSemana = 3;
+const diaDaSemana = 7;
+
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("Segunda-Feira");
+        break;
+    case 2:
+        console.log("Terça-Feira");
+        break;
+    case 3:
+        console.log("Quarta-Feira");
+        break;
+    case 4:
+        console.log("Quinta-Feira");
+        break;
+    case 5:
+        console.log("Sexta-Feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+}
