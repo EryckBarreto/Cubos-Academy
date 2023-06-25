60702e8a1d8de4f4cb7cd64ee80dddf686185988
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 20:14:24 -0300

    resolução do exercício "soma dos números pares"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/06/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/06/index.js
@@ -1,2 +1,12 @@
-const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+const numeros = [3, 4, 7, 8, 1, 6, 5, 10, 1000];
+
+let soma = 0
+
+for (let numero of numeros) {
+    if (numero % 2 === 0) {
+        soma += numero;
+    }
+}
+
+console.log(`A soma dos números pares é de ${soma}.`);
 
