a7996d0865aaed2ff8f0a7d48d338db0a92c6d31
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 20:33:17 -0300

    Resolução do exercício "encontrando o maior"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/08/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/08/index.js
@@ -1,1 +1,11 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numeros = [3, 24, 1, 8, 11, 7, 15, 33];
+
+let maior = 0;
+
+for (let numero of numeros) {
+    if (numero > maior) {
+        maior = numero;
+    }
+}
+
+console.log(`O maior número dessa lista é o ${maior}.`);
