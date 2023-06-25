5c30fe25ffe3d4e9c8cd6a555ff2060af4490614
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 17:24:49 -0300

    Introdução sobre arrays em JavaScript

---
+++ modulo1/arraysLoops/index.js
@@ -0,0 +1,8 @@
+const nomeDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+const notasDosEstudantes = [0, 10, 5, 7, 9];
+
+console.log(nomeDeEstudantes[0]);
+console.log(nomeDeEstudantes[1]);
+console.log(nomeDeEstudantes[2]);
+console.log(nomeDeEstudantes[3]);
+console.log(nomeDeEstudantes[4]);
