13d86a419171e5c7f85aeba1e68e8402cb08b6c4
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 22:32:32 -0300

    resolução do exercício "imprima os menores"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/14/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/14/index.js
@@ -1,3 +1,17 @@
-const arrayA = [5, 32, 3, 44, 1];
-const arrayB = [57, 4, 21, 2, 13];
+const arrayA = [5, 32, 3, 44, 1, 5, 34, 67, 1, 5];
+const arrayB = [57, 4, 21, 2, 13, 6, 7, 21, 5, 32];
+const arrayC = [];
 
+let numeroMenor = arrayA[0];
+
+for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] < arrayB[i]) {
+        numeroMenor = arrayA[i];
+        arrayC.push(arrayA[i]);
+    } else {
+        numeroMenor = arrayB[i];
+        arrayC.push(arrayB[i]);
+    }
+}
+
+console.log(arrayC);
