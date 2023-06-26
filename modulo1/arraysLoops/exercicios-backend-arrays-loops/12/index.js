53d6243be2dc038eb7e628c9457899a898b57931
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 22:19:40 -0300

    resolução do exercício "fila do banco"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/12/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,15 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+// const filaDeDentro = ["Jose", "Maria", "Joao"];
+// const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+
+const filaDeDentro = ["Jose", "Joao"];
+const filaDeFora = ["Joana", "Roberta"];
+
+
+while (filaDeDentro.length < 5 && filaDeFora.length > 0) {
+    let primeiraFilaDeFora = filaDeFora.shift();
+    filaDeDentro.push(primeiraFilaDeFora);
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
 
