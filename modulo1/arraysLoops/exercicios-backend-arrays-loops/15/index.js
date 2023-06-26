09ffc88e9546c786b451071f1aa004609161305c
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 22:41:59 -0300

    resolução do exercício separando pares de ímpares

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/15/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/15/index.js
@@ -2,3 +2,33 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+// let i = 0;
+
+// while (i < original.length) {
+//     if (original[i] % 2 === 0) {
+//         pares.push(original[i]);
+//     } else {
+//         impares.push(original[i]);
+//     }
+//     i++;
+// }
+
+// for (let i = 0; i < original.length; i++) {
+//     if (original[i] % 2 === 0) {
+//         pares.push(original[i]);
+//     } else {
+//         impares.push(original[i]);
+//     }
+// }
+
+for (let numero of original) {
+    if (numero % 2 === 0) {
+        pares.push(numero);
+    } else {
+        impares.push(numero);
+    }
+}
+
+console.log(pares);
+console.log(impares);
+
