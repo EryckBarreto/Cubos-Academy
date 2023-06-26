be74d6b57039ae8e2a6f0e9d516d018ff6db5b49
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-25 21:13:12 -0300

    resolução do exercício "filtrando números"

--- modulo1/arraysLoops/exercicios-backend-arrays-loops/11/index.js
+++ modulo1/arraysLoops/exercicios-backend-arrays-loops/11/index.js
@@ -1,2 +1,11 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 13, 17, 26, 34, 118, 245, 14, 11, 655];
+const intervaloArray = [];
+
+for (let numero of original) {
+    if (numero >= 10 && numero <= 20 || numero > 100)
+    intervaloArray.push(numero);
+}
+
+console.log(intervaloArray);
+
 
