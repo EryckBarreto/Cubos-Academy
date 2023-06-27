1f3e37d732f0ec46ee191d912d91a822044ec58e
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 16:55:29 -0300

    resolucão do exercício 2 proposto em aula sobre objetos

---
+++ modulo1/objetos/exercicio2.js
@@ -0,0 +1,27 @@
+const produtosConsumidos = [
+    {
+        nomeProduto: "arroz",
+        precoUnitarioCentavos: 1000,
+        quantidadeComprada: 1
+    },
+
+    {
+        nomeProduto: "feijão",
+        precoUnitarioCentavos: 700,
+        quantidadeComprada: 3
+    },
+
+    {
+        nomeProduto: "sal",
+        precoUnitarioCentavos: 500,
+        quantidadeComprada: 2
+    }
+]
+
+const cartaoDeConsumo = {
+    nomeCliente: "Ryu",
+    idadeCliente: 30,
+    produtosConsumidos
+}
+
+console.log(cartaoDeConsumo);
