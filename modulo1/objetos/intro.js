bf025f0628cae254a9a99e867d35a0c6db1ca00a
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 15:26:54 -0300

    introdução ao uso de objeo em javascript

---
+++ modulo1/objetos/intro.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: "Eryck",
+    altura: 1.71,
+    peso: 98
+};
+
+pessoa.nome = "Pedro";
+
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura}m e peso ${pessoa.peso}kg.`);
