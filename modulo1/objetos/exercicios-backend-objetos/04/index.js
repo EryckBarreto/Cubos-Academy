4c47622e6ec452c45d754d585939f46adc4a6a74
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 20:18:22 -0300

    resolução do exercício "adicionando maior idade"

---
+++ modulo1/objetos/exercicios-backend-objetos/04/index.js
@@ -0,0 +1,28 @@
+const usuarios = [
+    {
+        nome: "João",
+        idade: 25,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+    },
+    {
+        nome: "Beatriz",
+        idade: 15,
+    },
+    {
+        nome: "Carlos",
+        idade: 16,
+    },
+    {
+        nome: "Antonio",
+        idade: 32,
+    },
+]
+
+for (let usuario of usuarios) {
+    usuario.maior_idade = usuario.idade > 17;
+}
+
+console.log(usuarios);
