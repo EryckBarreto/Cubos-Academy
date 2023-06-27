dda1de3824c7b997b2beee3ff562ff49672f2efe
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 19:41:29 -0300

    resolução do exercício "lista de veículos"

---
+++ modulo1/objetos/exercicios-backend-objetos/02/index.js
@@ -0,0 +1,26 @@
+const carros = [
+    {
+        marca: "Honda",
+        modelo: "Fit",
+        ano: 2009,
+        cor: "Preto",
+        quantidade_portas: 4,
+        automatico: false
+    },
+    {
+        marca: "Gol",
+        modelo: "Quadrado",
+        ano: 1980,
+        cor: "Azul",
+        quantidade_portas: "2",
+        automatico: false
+    },
+    {
+        marca: "Mitsubshi",
+        modelo: "Eclipe",
+        ano: 2005,
+        cor: "Vermelho",
+        quantidade_portas: 4,
+        automatico: true
+    }
+];
