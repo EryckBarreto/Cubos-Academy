3d5a1a5822eb6592f154290f25792506a70d9bbf
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 19:32:53 -0300

    resolução do exercício "cadastro de endereço"

---
+++ modulo1/objetos/exercicios-backend-objetos/01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "José Loureiro",
+    numero: 1045,
+    complemento: "fundos",
+    bairro: "centro",
+    cep: "80010-000",
+    cidade: "Curitiba",
+    estado: "Paraná",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Ryu",
+    email: "ryu@ryu.com",
+    telefone: "4136216655",
+    data_nascimento: "21/02/1999",
+    endereco
+}
+
+console.log(usuario);
