980d3f6d02214cde2eefa623e7d73b106754380b
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 20:26:20 -0300

    resolução do exercício "cadastro de aulas"

---
+++ modulo1/objetos/exercicios-backend-objetos/05/index.js
@@ -0,0 +1,27 @@
+const aulas = [
+    {
+        identificador: 1,
+        nome_da_aula: "Às vezes dá vontade de chorar"
+    },
+    {
+        identificador: 2,
+        nome_da_aula: "Às vezes de se jogar na frente de um caminhão"
+    },
+    {
+        identificador: 3,
+        nome_da_aula: "Porém quando dá certo"
+    },
+    {
+        identificador: 4,
+        nome_da_aula: "Parece que nada é impossível KK"
+    }
+]
+
+
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas
+}
+
+console.log(curso);
