b584b3e70932de2481aef04c0e229fe58927bde9
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 16:34:10 -0300

    aula sobre utilização de shorthand em objetos

---
+++ modulo1/objetos/shorthand.js
@@ -0,0 +1,17 @@
+const carro = {
+    marca: "Toyota",
+    modelo: "Etios",
+    ano: 2020,
+    potencia: 80
+};
+
+const pessoa = {
+    nome: "José",
+    altura: 1.73,
+    peso: 60,
+    carro
+};
+
+
+
+console.log(pessoa);
