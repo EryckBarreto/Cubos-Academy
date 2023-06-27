05f84fe5561654cb5c120fc48c3013b4482bf81a
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-27 19:48:01 -0300

    resolução do exercício "mensagem de apresentação"

---
+++ modulo1/objetos/exercicios-backend-objetos/03/index.js
@@ -0,0 +1,11 @@
+const usuario = {
+    nome: "Eryck",
+    idade: 30,
+    profissao: "desenvolvedor back-end",
+    cor_preferida: "lilás",
+};
+
+const {nome, idade, profissao, cor_preferida} = usuario;
+
+
+console.log(`Olá... Eu sou ${nome} e tenho ${idade} anos de idade. Sou ${profissao} e minha cor preferida é ${cor_preferida}!`);
