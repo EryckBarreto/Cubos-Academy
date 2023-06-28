c984b68b36d3cae549965c71d17b419325032953
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-28 17:03:54 -0300

    resolução do exercício "encontrado Carlos na multidão"

---
+++ modulo1/objetos/exercicios-backend-objetos/09/index.js
@@ -0,0 +1,28 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+]
+
+for (let i = 0; i < participantes.length; i++) {
+    if (participantes[i].nome == "Pedro") {
+        console.log(`Galera, o ${participantes[i].nome} está na posição ${participantes.length} da fila.`);
+    }
+
+}
+
