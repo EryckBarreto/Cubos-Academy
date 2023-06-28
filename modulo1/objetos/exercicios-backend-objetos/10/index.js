ece148a406738fb93fc22d38b941e919cb2860ae
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-28 20:01:57 -0300

    resolução do exercício "compras online"

---
+++ modulo1/objetos/exercicios-backend-objetos/10/index.js
@@ -0,0 +1,126 @@
+const patricia = { nome: "Patricia", carrinho: [] }
+const carlos = { nome: "Carlos", carrinho: [] }
+const renato = { nome: "Renato", carrinho: [] }
+const jose = { nome: "José", carrinho: [] }
+const roberto = { nome: "Roberto", carrinho: [] }
+
+// produtos
+const tv = { nome: "TV Samsung 4K", valorEmCentavos: 129900 }
+const notebook = { nome: "Notebook Dell", valorEmCentavos: 399990 }
+const mouse = { nome: "Mouse MX Master 3", valorEmCentavos: 23000 }
+const teclado = { nome: "Teclado Keychron K8", valorEmCentavos: 50000 }
+const caboUsb = { nome: "Cabo USB 2 Metros", valorEmCentavos: 1990 }
+const carregador = { nome: "Carregador portátil", valorEmCentavos: 4590 }
+const webcam = { nome: "Webcam C920s", valorEmCentavos: 80000 }
+const monitor = { nome: "Monitor LG 29 FHD", valorEmCentavos: 129900 }
+
+
+
+jose.carrinho.push(
+    {
+      item: {
+        nome: tv.nome,
+        valorEmCentavos: tv.valorEmCentavos
+      },
+      quantidade: 1
+    },
+    {
+      item: {
+        nome: caboUsb.nome,
+        valorEmCentavos: caboUsb.valorEmCentavos
+      },
+      quantidade: 2
+    },
+    {
+      item: {
+        nome: webcam.nome,
+        valorEmCentavos: webcam.valorEmCentavos
+      },
+      quantidade: 1
+    }
+  );
+
+  carlos.carrinho.push(
+    {
+      item: {
+        nome: notebook.nome,
+        valorEmCentavos: notebook.valorEmCentavos
+      },
+      quantidade: 1
+    }
+  );
+
+  patricia.carrinho.push(
+    {
+      item: {
+        nome: teclado.nome,
+        valorEmCentavos: teclado.valorEmCentavos
+      },
+      quantidade: 1
+    },
+    {
+      item: {
+        nome: caboUsb.nome,
+        valorEmCentavos: caboUsb.valorEmCentavos
+      },
+      quantidade: 2
+    },
+    {
+        item: {
+          nome: carregador.nome,
+          valorEmCentavos: carregador.valorEmCentavos
+        },
+        quantidade: 1
+      },
+    {
+      item: {
+        nome: mouse.nome,
+        valorEmCentavos: mouse.valorEmCentavos
+      },
+      quantidade: 1
+    },
+    {
+        item: {
+          nome: monitor.nome,
+          valorEmCentavos: monitor.valorEmCentavos
+        },
+        quantidade: 1
+      },
+  );
+
+  renato.carrinho.push(
+    {
+      item: {
+        nome: webcam.nome,
+        valorEmCentavos: webcam.valorEmCentavos
+      },
+      quantidade: 5
+    }
+  );
+
+  roberto.carrinho.push(
+    {
+      item: {
+        nome: webcam.nome,
+        valorEmCentavos: webcam.valorEmCentavos
+      },
+      quantidade: 1
+    },
+    {
+      item: {
+        nome: caboUsb.nome,
+        valorEmCentavos: caboUsb.valorEmCentavos
+      },
+      quantidade: 2
+    },
+    {
+      item: {
+        nome: monitor.nome,
+        valorEmCentavos: monitor.valorEmCentavos
+      },
+      quantidade: 1
+    }
+  );
+
+
+  console.log(roberto.carrinho);
