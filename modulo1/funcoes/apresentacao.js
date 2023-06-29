683c85dae81a0962f15627ffbd3ddc4add60d515
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-29 15:25:18 -0300

    apresentação do conceito de função e do porquê usá-lo

---
+++ modulo1/funcoes/apresentacao.js
@@ -0,0 +1,24 @@
+const pessoa = {
+    nome: "Eryck",
+    idade: 30,
+    profissao: "professor"
+}
+
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos e sou ${pessoa.profissao}.`);
+
+const pessoa2 = {
+    nome: "Pedro",
+    idade: 65,
+    profissao: "médico"
+}
+
+console.log(`Meu nome é ${pessoa2.nome}, tenho ${pessoa2.idade} anos e sou ${pessoa2.profissao}.`);
+
+const pessoa3 = {
+    nome: "Ana",
+    idade: 19,
+    profissao: "estudante"
+}
+
+console.log(`Meu nome é ${pessoa3.nome}, tenho ${pessoa3.idade} anos e sou ${pessoa3.profissao}.`);
+
