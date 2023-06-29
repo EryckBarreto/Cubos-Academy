ef180638ded8aa3390ff02aa182f9e59de325951
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-29 15:40:35 -0300

    aula sobre uso dos parâmetros na função

---
+++ modulo1/funcoes/funcaoComParametros.js
@@ -0,0 +1,7 @@
+function cumprimentar(pessoa, idade) { // pessoa só existe dentro da função
+    console.log(`Olá, ${pessoa}! Fiquei sabendo que você tem ${idade} anos.`);
+}
+
+cumprimentar("José", 20);
+cumprimentar("João", 34);
+cumprimentar("Ana");
