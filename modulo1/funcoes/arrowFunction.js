cb5649803cced95bc9d36dca427a4b9971df68d3
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-29 19:52:45 -0300

    aula sobre arrow function em JavaScript

---
+++ modulo1/funcoes/arrowFunction.js
@@ -0,0 +1,13 @@
+// declaração tradicional
+// function soma(x, y) {
+//     const resultado = x + y;
+//     return resultado;
+// } 
+
+// arrow function
+const soma = (x, y) => {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(5,2));
