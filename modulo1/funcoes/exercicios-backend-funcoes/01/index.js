6edbfc2bb99b6c53c96b7f1bc1c86dcaa238ccc4
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-29 21:09:29 -0300

    resolução do exercício "sistema de correção de provas"

--- modulo1/funcoes/exercicios-backend-funcoes/01/index.js
+++ modulo1/funcoes/exercicios-backend-funcoes/01/index.js
@@ -20,9 +20,35 @@ const prova = {
             correta: "b"
         },
         {
-            resposta: "c",
+            resposta: "d",
             correta: "c"
         }
-    ]
+    ],
+    corrigirProva: function () {
+        {
+            somaQuestaoCerta = 0;
+            for (let questao of this.questoes) {
+                if (questao.resposta === questao.correta) {
+                    somaQuestaoCerta++;
+                }
+            }
+            return `O aluno(a) ${this.aluno} acertou ${somaQuestaoCerta} questões e obteve nota ${somaQuestaoCerta * 2}.`;
+        }
+    }
 };
 
+
+
+// function corrigirProva (prova) {
+//     somaQuestaoCerta = 0;
+//     for (let questao of prova.questoes) {
+//         if (questao.resposta === questao.correta) {
+//             somaQuestaoCerta++;
+//         }
+//     }
+//     return `O aluno(a) ${prova.aluno} acertou ${somaQuestaoCerta} questões e obteve nota ${somaQuestaoCerta * 2}.`;
+// }
+
+// console.log(`O aluno(a) ${prova.aluno} acertou ${corrigirProva(prova)} questões e obteve nota ${corrigirProva(prova) * 2}.`);
+
+console.log(prova.corrigirProva());
