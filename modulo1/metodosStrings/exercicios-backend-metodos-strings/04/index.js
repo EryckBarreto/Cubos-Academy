29eeb795cae880694e9aaa56f16dd8e723a11a7a
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 18:43:36 -0300

    resolução da questão "sistema de formatação de dados do formulário"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/04/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/04/index.js
@@ -1,3 +1,25 @@
 let identificador = "123";
+let identificadorFormatado = "";
 let nome = "José silva costa";
+const nomeLista = nome.split(" ");
+let nomeFormatado = "";
 let email = "      jose@email.com  ";
+let emailFormatado = email.trim();
+
+
+if (identificador.length < 6) {
+    identificadorFormatado = identificador.padStart(6, "0");
+}
+
+for (let i = 0; i < nomeLista.length; i++) {
+    nomeFormatado += nomeLista[i][0].toUpperCase() + nomeLista[i].slice(1) + " ";
+}
+
+
+
+
+
+console.log(identificadorFormatado);
+console.log(nomeFormatado);
+console.log(emailFormatado);
+
