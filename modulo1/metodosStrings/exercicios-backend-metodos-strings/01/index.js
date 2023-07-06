b71976601744cd3c92ee2c5660898d67f23e4a18
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 15:41:01 -0300

    resolução do exercício "sistema de filtro de comentários"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/01/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,9 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse pandemia é muito perigoso!";
+
+let comentarioToLowerCase = comentario.toLocaleLowerCase();
+
+if (comentarioToLowerCase.includes("covid") || comentarioToLowerCase.includes("pandemia")) {
+    console.log("Comentário bloqueado por conter palavras proibidas.");
+} else {
+    console.log("Comentário autorizado.");
+}
