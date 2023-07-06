50e66f01694dcec77f751f7506fcaf2a816148f3
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 17:11:07 -0300

    resolução do exercício "url amigável"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/03/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,9 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+const textoLetrasMinusculas = texto.toLocaleLowerCase();
+let textoFormatado = "";
+
+for (let letra of textoLetrasMinusculas) {
+    textoFormatado += letra.replace(" ", "-");
+}
+
+console.log(textoFormatado);
