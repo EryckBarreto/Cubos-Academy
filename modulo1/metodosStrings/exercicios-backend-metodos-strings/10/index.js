2036c68662be42254d0671892be1ab9aa784c3d8
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 20:25:49 -0300

    resolução do exercício "validação de arquivos"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/10/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/10/index.js
@@ -1,1 +1,13 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.gif';
+
+function validacaoDeArquivos(nomeArquivo) {
+
+    if (nomeArquivo.includes("pdf") || nomeArquivo.includes("jpeg") || nomeArquivo.includes("gif") || nomeArquivo.includes("png")) {
+        return `Arquivo válido.`;
+    } else {
+        return `Arquivo inválido, só são aceitos arquivos jpg, jpeg, gif ou png.`;
+    }
+
+}
+
+console.log(validacaoDeArquivos(nomeArquivo));
