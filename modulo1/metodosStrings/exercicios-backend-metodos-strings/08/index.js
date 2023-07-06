5008c49afe884d0420fbf597461991b7f72c4b2e
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 20:07:56 -0300

    resolução do exercício "remover pontuação"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/08/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/08/index.js
@@ -1,1 +1,15 @@
-const cpf = "011.022.033-44";
+const cpf = "596.587.588-46";
+
+function cpfSemNumero(cpf) {
+    cpfFormatado = "";
+    for (let caractere of cpf) {
+        if (caractere === "." || caractere === "-") {
+            caractere = "";
+        }
+        cpfFormatado += caractere;
+    }
+    return cpfFormatado;
+}
+
+console.log(cpfSemNumero(cpf));
+
