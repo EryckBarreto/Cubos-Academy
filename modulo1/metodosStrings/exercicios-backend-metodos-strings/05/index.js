eadbc4485ee96bc5b23c4a503871cec02b3dcc02
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 19:10:00 -0300

    resolução do exercício "esconder número do cartão de crédito"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/05/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/05/index.js
@@ -1,1 +1,7 @@
 const numeroCartao = '1111222233334444';
+const primeiraParte = numeroCartao.slice(0, 4);
+const segundaParte = numeroCartao.slice(12);
+const asteriscos = "********"
+const numeroCaracteresOcultos = primeiraParte + asteriscos + segundaParte;
+
+console.log(numeroCaracteresOcultos);
