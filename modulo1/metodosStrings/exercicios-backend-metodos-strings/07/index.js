41de42e4c17861d82ecc45712b9955ffa6cf37e7
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 20:03:07 -0300

    resolução do exercício "validação de e-mail"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/07/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/07/index.js
@@ -1,1 +1,7 @@
 const email = "aluno@cubos.academy";
+
+if (!email.startsWith(".") && !email.endsWith(".") && email.includes("@") && email.includes(".")) {
+    console.log("Email válido");
+} else {
+    console.log("Email inválido.");
+}
