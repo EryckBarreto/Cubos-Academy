8e49d0d39422ed4e085ed9e0720e8745cbfd588d
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-06 20:18:32 -0300

    resolução das questão "gerador de nickname"

--- modulo1/metodosStrings/exercicios-backend-metodos-strings/09/index.js
+++ modulo1/metodosStrings/exercicios-backend-metodos-strings/09/index.js
@@ -1,1 +1,20 @@
-const nome = 'Guido Cerqueira';
+const nome = 'Eryck Barreto';
+
+function geradorNickname(nome) {
+    let primeiroCaractere = "@";
+    let nickname = "";
+
+    for (let letra of nome) {
+        if (letra.replace(" ", "")) {
+        nickname += letra;
+        }
+        if (nickname.length === 12) {
+            break;
+        }
+    }
+     const nickFormatado = primeiroCaractere + nickname;
+
+    return nickFormatado.toLowerCase();
+}
+
+console.log(geradorNickname(nome));
