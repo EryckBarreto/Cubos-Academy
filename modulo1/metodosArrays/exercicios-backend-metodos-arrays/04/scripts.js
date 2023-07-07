a41c574b153dba4b306e98f84c75a850ef18a49f
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-07 20:26:21 -0300

    resolução do exercício "fila de atendimento"

--- modulo1/metodosArrays/exercicios-backend-metodos-arrays/04/scripts.js
+++ modulo1/metodosArrays/exercicios-backend-metodos-arrays/04/scripts.js
@@ -1,1 +1,5 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function filaDeAtendimento(todosParcientes, atender, agendar) {
+    //ENUNCIADO DA QUESTÂO INCOMPLETA, NÂO SENDO POSSÍVEL CONCLUIR A QUESTÃO
+};
