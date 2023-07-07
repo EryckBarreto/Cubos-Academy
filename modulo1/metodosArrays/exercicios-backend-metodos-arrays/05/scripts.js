af774dbb93922833ddde103011056feff75d2e06
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-07 20:55:41 -0300

    resolução do exercício "fila de atendimento 2.0"

--- modulo1/metodosArrays/exercicios-backend-metodos-arrays/05/scripts.js
+++ modulo1/metodosArrays/exercicios-backend-metodos-arrays/05/scripts.js
@@ -1,1 +1,32 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function agendarPaciente(pacientes, paciente) {
+    for (let i = 0; i < pacientes.length; i++) {
+        if (pacientes[i] === paciente) {
+            const pacienteAgendado = pacientes.splice(i, 1);
+            pacientes.push(pacienteAgendado[0]);
+            return;
+        }
+    }
+}
+
+function atenderPaciente(pacientes) {
+            pacientes.splice(0, 1);
+            return;
+}
+
+function cancelarPaciente(pacientes, paciente) {
+    for (let i = 0; i < pacientes.length; i++) {
+        if (pacientes[i] === paciente) {
+            pacientes.splice(i, 1);
+            return;
+        }
+    }    
+}
+
+agendarPaciente(pacientes, 'Pedro');
+console.log(pacientes);
+atenderPaciente(pacientes);
+console.log(pacientes);
+cancelarPaciente(pacientes, 'João');
+console.log(pacientes);
