77deb488fe8a89ddd8bdeea4c0fe2fa2c0b2d98c
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-08 18:34:34 -0300

    aula sobre callback em javascript

---
+++ modulo1/metodosArrays2/1.callback.js
@@ -0,0 +1,19 @@
+// const imprimirNome = (nome) => {
+//     console.log(nome);
+// }
+
+// imprimirNome("Guido");
+
+const imprimirGuido = () => {
+    console.log("Guido Cerqueira");
+}
+
+// setTimeout(imprimirGuido, 5000);
+
+// setTimeout(() => {
+//     console.log("Cubos Academy");
+// }, 2000)
+
+setTimeout(function(){
+    console.log('Cubos Academy');
+}, 2000)
