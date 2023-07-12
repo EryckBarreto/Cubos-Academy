b9a99aefe36afb19bef9d8f941dd2e7c9a75e261
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-12 20:57:54 -0300

    aula sobre como utilizar o reduce em array dentro de outro array

---
+++ modulo1/metodosArrays3/6.reduceArrayDeArray.js
@@ -0,0 +1,12 @@
+const array = [
+    [0,1],
+    [2,3,4],
+    [14,22,98]
+];
+
+const arrayReduzido = array.reduce((acumulador, elementoAtual, indice, array) => {
+    acumulador = [ ...acumulador, ...elementoAtual];
+    return acumulador;
+}, []);
+
+console.log(arrayReduzido);
