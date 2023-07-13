920f16e867b2503c27180009d928bf496510697e
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-12 21:09:07 -0300

    resolução de exercício proposto em aula

---
+++ modulo1/metodosArrays3/7.exercicio-resolvido.js
@@ -0,0 +1,18 @@
+const usuarios = [
+    { id: 11, nome: "joao", idade: 23 },
+    { id: 2, nome: "maria", idade: 18 },
+    { id: 4, nome: "ana", idade: 30 },
+    { id: 1, nome: "rodrigo", idade: 17 },
+    { id: 123, nome: "rodrigo", idade: 17 },
+]
+
+const maiorIdade = usuarios.reduce((acumulador, elementoAtual, indice, lista) => {
+    let maior = acumulador;
+    
+    if (elementoAtual.idade > maior.idade) {
+        maior = elementoAtual;
+    }
+    return maior;
+});
+
+console.log(maiorIdade);
