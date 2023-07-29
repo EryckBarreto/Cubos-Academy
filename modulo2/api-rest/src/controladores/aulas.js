7e0106269e5adac36644ddc85e755889374315ba
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-29 19:44:09 -0300

    adicionada a possibilidade de adicionar aulas para um instrutor e listá-las

--- modulo2/api-rest/src/controladores/aulas.js
+++ modulo2/api-rest/src/controladores/aulas.js
@@ -26,6 +26,45 @@ const cadastrarAula = (requisicao, resposta) => {
 
 };
 
+const listarAulas = (requisicao, resposta) => {
+    return resposta.status(200).json(aulas);
+};
+
+const obterAula = (requisicao, resposta) => {
+    const {id} = requisicao.params;
+
+    const aula = aulas.find((aula) => {
+        return aula.id === Number(id);
+    });
+
+    if (!aula) {
+        return resposta.status(404).json({mensagem: "Aula não encontrada"});
+    }
+
+    return resposta.status(200).json(aula);
+};
+
+const obterAulasInstrutor = (requisicao, resposta) => {
+    const {idInstrutor} = requisicao.params;
+
+    const instrutor = instrutores.find((instrutor) => {
+        return instrutor.id === Number(idInstrutor);
+    });
+
+    if (!instrutor) {
+        return resposta.status(404).json({mensagem: "O instrutor não existe."})
+    };
+
+    const aulasEncontradas = aulas.filter((aula) => {
+        return aula.instrutor_id === instrutor.id;
+    });
+
+    return resposta.status(200).json(aulasEncontradas);
+};
+
 module.exports = {
-    cadastrarAula
+    cadastrarAula,
+    listarAulas,
+    obterAula,
+    obterAulasInstrutor
 }
--- modulo2/api-rest/src/rotas.js
+++ modulo2/api-rest/src/rotas.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const rotas = express();
 const {listarInstrutores, obterInstrutor, cadastrarInstrutor, atualizarInstrutor, atualizarStatusInstrutor, deletarInstrutor} = require("./controladores/instrutores");
-const {cadastrarAula} = require("./controladores/aulas")
+const {cadastrarAula, listarAulas, obterAula, obterAulasInstrutor} = require("./controladores/aulas")
 
 rotas.get("/instrutores", listarInstrutores);
 rotas.get("/instrutores/:id", obterInstrutor);
 rotas.post("/instrutores", cadastrarInstrutor);
 rotas.put("/instrutores/:id", atualizarInstrutor);
 rotas.patch("/instrutores/:id/status", atualizarStatusInstrutor);
-rotas.delete("/instrutores/:id", deletarInstrutor)
+rotas.delete("/instrutores/:id", deletarInstrutor);
 rotas.post("/instrutores/:idInstrutor/aulas", cadastrarAula);
+rotas.get("/aulas", listarAulas);
+rotas.get("/aulas/:id", obterAula);
+rotas.get("/instrutores/:idInstrutor/aulas", obterAulasInstrutor);
 
 module.exports = rotas;
