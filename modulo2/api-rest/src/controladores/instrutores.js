5168f0e747829a4387d565bf9ffb0317d9a14855
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-29 16:08:38 -0300

    implementada rota de atualização pelo verbo PUT

--- modulo2/api-rest/src/controladores/instrutores.js
+++ modulo2/api-rest/src/controladores/instrutores.js
@@ -10,38 +10,68 @@ const obterInstrutor = (requisicao, resposta) => {
 
     const instrutor = instrutores.find((instrutor) => {
         return instrutor.id === Number(id);
-    })
+    });
 
     if (!instrutor) {
         return resposta.status(404).json({mensagem: "Instrutor não encontrado."});
-    }
+    };
+
     return resposta.status(200).json(instrutor);
-}
+};
 
 const cadastrarInstrutor = (requisicao, resposta) => {
     const {nome, email, status} = requisicao.body;
 
     if (!nome) {
         return resposta.status(400).json({mensagem: "O nome é obrigatório."});
-    }
+    };
 
     if (!email) {
         return resposta.status(400).json({mensagem: "O email é obrigatório."});
-    }
+    };
 
     const instrutor = {
         id: identificadorInstrutor++,
         nome,
         email,
         status: status ?? true
-    }
+    };
 
     instrutores.push(instrutor);
     return resposta.status(201).json(instrutor);
 };
 
+const atualizarInstrutor = (requisicao, resposta) => {
+    const {id} = requisicao.params;
+    const {nome, email, status} = requisicao.body;
+
+    if (!nome) {
+        return resposta.status(400).json({mensagem: "O nome é obrigatório."});
+    };
+
+    if (!email) {
+        return resposta.status(400).json({mensagem: "O email é obrigatório."});
+    };
+
+    const instrutor = instrutores.find((instrutor) => {
+        return instrutor.id === Number(id);
+    });
+
+    if (!instrutor) {
+        return resposta.status(404).json({mensagem: "Instrutor não encontrado."});
+    };
+
+    instrutor.nome = nome;
+    instrutor.email = email;
+    instrutor.status = status;
+
+    return resposta.status(203).send();
+
+};
+
 module.exports = {
     listarInstrutores,
     obterInstrutor,
-    cadastrarInstrutor
+    cadastrarInstrutor,
+    atualizarInstrutor
 };
--- modulo2/api-rest/src/rotas.js
+++ modulo2/api-rest/src/rotas.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const rotas = express();
-const {listarInstrutores, obterInstrutor, cadastrarInstrutor} = require("./controladores/instrutores")
+const {listarInstrutores, obterInstrutor, cadastrarInstrutor, atualizarInstrutor} = require("./controladores/instrutores")
 
 rotas.get("/instrutores", listarInstrutores);
 rotas.get("/instrutores/:id", obterInstrutor)
 rotas.post("/instrutores", cadastrarInstrutor);
+rotas.put("/instrutores/:id", atualizarInstrutor);
+
+
 module.exports = rotas;
