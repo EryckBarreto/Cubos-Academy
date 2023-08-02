317bd8c472002b9c926cc6e17536652c79c7bebb
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-02 11:07:43 -0300

    explicação entre a diferença de uma programação síncrona e assíncrona

---
+++ modulo2/programacao-assincrona/scripts.js
@@ -0,0 +1,17 @@
+const {getCityFromZipcode} = require("utils-playground")
+
+// console.log("inicio");
+
+// for (let index = 1; index <= 5; index++) {
+//     console.log(index);
+// };
+
+// console.log("fim");
+
+console.log("inicio");
+
+getCityFromZipcode("80010000").then(cidade => {
+    console.log(cidade);
+});
+
+console.log("fim");
