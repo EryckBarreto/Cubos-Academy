74a8fcd3a80d07acc4f4eda0a6513e5cd714719d
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-02 15:53:00 -0300

    uso do Promise all e desestruturação de array com respostas do promise

--- modulo2/programacao-assincrona/api-funcoes-assincronas/src/index.js
+++ modulo2/programacao-assincrona/api-funcoes-assincronas/src/index.js
@@ -3,8 +3,16 @@ const {getCityFromZipcode} = require("utils-playground")
 const app = express();
 
 app.get("/", async (requisicao, resposta) => {
-    const cidade = await getCityFromZipcode("80010000");
-    resposta.send(`A cidade encontrada foi: ${cidade}`);
+    const cidade = getCityFromZipcode("80010000");
+    const cidade2 = getCityFromZipcode("83404010");
+
+    const promise = await Promise.all([cidade, cidade2]);
+
+    // console.log(promise);
+
+    const [resposta1, resposta2] = promise;
+
+    resposta.send(`A cidade encontrada foi: ${resposta1} e ${resposta2} `);
 
 });
 
