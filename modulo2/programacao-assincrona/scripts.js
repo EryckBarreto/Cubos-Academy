acb2c9f97bcf8f1527517b1a6994eaf9e0a8d7e7
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-02 11:11:36 -0300

    explicação sobre promises e seus estados pendente, resolvida ou rejeitada

--- modulo2/programacao-assincrona/scripts.js
+++ modulo2/programacao-assincrona/scripts.js
@@ -8,10 +8,14 @@ const {getCityFromZipcode} = require("utils-playground")
 
 // console.log("fim");
 
-console.log("inicio");
+// console.log("inicio");
 
-getCityFromZipcode("80010000").then(cidade => {
-    console.log(cidade);
-});
+// getCityFromZipcode("80010000").then(cidade => {
+//     console.log(cidade);
+// });
 
-console.log("fim");
+// console.log("fim");
+
+const cidade = getCityFromZipcode("80010000");
+
+console.log(cidade);
