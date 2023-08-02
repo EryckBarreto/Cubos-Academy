9eea6598bd4d21aaed0e22844118d2477be302c2
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-02 15:29:13 -0300

    aula sobre uso do async e await para escrever códigos assíncronos de forma síncrona

--- modulo2/programacao-assincrona/scripts2.js
+++ modulo2/programacao-assincrona/scripts2.js
@@ -10,13 +10,35 @@ const { getCityFromZipcode, getStateFromZipcode } = require("utils-playground");
 //     console.log(erro.message);
 // })
 
-getCityFromZipcode("80010000").then(cidade => {
-    console.log(cidade);
-    getStateFromZipcode("80010000").then(uf => {
-        console.log(uf);
-    });
-}).catch((erro) => {
-    console.log(erro);
-});
+// getCityFromZipcode("80010000").then(cidade => {
+//     console.log(cidade);
+//     getStateFromZipcode("80010000").then(uf => {
+//         console.log(uf);
+//     });
+// }).catch((erro) => {
+//     console.log(erro);
+// });
 
+(async function() {
+
+    const cidade = await getCityFromZipcode("80010000");
+    console.log(cidade);
+
+    const estado = await getStateFromZipcode("80010000");
+    console.log(estado);
+
+})();
+
+//padrão normal
+// async function teste() {
+//     return
+// };
+
+//arrow function
+
+const teste = async () => {
+    return
+};
+
+console.log(teste());
 
