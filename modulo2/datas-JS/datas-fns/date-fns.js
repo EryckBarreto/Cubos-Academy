0d10140af519cae91d1342814c079918c7d79ebe
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-06 16:59:51 -0300

    uso da função add em date-fns

--- modulo2/datas-JS/datas-fns/date-fns.js
+++ modulo2/datas-JS/datas-fns/date-fns.js
@@ -1,5 +1,21 @@
-const { format } = require("date-fns");
+// const { format } = require("date-fns");
 
-const date = new Date(2017, 3, 12);
+// const date = new Date(2017, 3, 12);
 
-console.log(format(date, "dd 'de' MMM 'de' yyyy"));
+// console.log(format(date, "dd 'de' MMM 'de' yyyy"));
+
+// ----------------------------------
+
+const { add } = require("date-fns");
+
+const date = new Date(2018, 2, 13, 12);
+
+console.log(date);
+
+console.log(add(date , {
+    seconds: 2,
+    days: 1,
+    years: 10
+}));
+
+console.log(date);
