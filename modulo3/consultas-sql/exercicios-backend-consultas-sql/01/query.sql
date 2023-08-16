9c43edc5f58f7d8ac578e0b22bc3db6aad3259b1
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-16 20:10:37 -0300

    resolução dos exercícios de consulta SQL

---
+++ modulo3/consultas-sql/exercicios-backend-consultas-sql/01/query.sql
@@ -0,0 +1,109 @@
+----------1
+
+SELECT compositor, id 
+FROM musicas;
+
+----------2
+
+SELECT compositor, tempo 
+FROM musicas
+WHERE tempo > 60*4;
+
+----------3
+
+SELECT compositor, composicao, id 
+FROM musicas
+WHERE id BETWEEN 47 AND 123;
+
+----------4
+
+SELECT * 
+FROM musicas
+WHERE compositor IS NOT NULL
+AND tempo < 60*5 AND compositor <> 'Bach';
+
+----------5
+
+SELECT composicao, tempo, compositor 
+FROM musicas
+WHERE compositor = 'Mozart' OR compositor = 'Bach';
+
+----------6
+
+SELECT * 
+FROM musicas
+ORDER BY id DESC;
+
+----------7
+
+SELECT *
+FROM musicas
+ORDER BY tempo DESC;
+
+----------8
+
+SELECT *
+FROM musicas
+ORDER BY tempo ASC
+LIMIT 5;
+
+----------9
+
+SELECT *
+FROM musicas
+ORDER BY tempo DESC
+LIMIT 10;
+
+----------10
+
+SELECT *
+FROM musicas
+ORDER BY tempo ASC
+LIMIT 10 OFFSET 5;
+
+----------11
+
+SELECT *
+FROM musicas
+LIMIT 10 OFFSET 30;
+
+----------12
+
+SELECT *
+FROM musicas
+LIMIT 12 OFFSET 60;
+
+----------13
+
+SELECT DISTINCT compositor
+FROM musicas
+WHERE compositor IS NOT NULL;
+
+----------14
+
+SELECT DISTINCT compositor, composicao
+FROM musicas;
+
+----------15
+
+SELECT *
+FROM musicas
+WHERE compositor LIKE 'Bra%';
+
+----------16
+
+SELECT *
+FROM musicas
+WHERE ritmo ILIKE '%troppo';
+
+----------17
+
+SELECT *
+FROM musicas
+WHERE composicao ILIKE '%quartet%';
+
+----------18
+
+SELECT *
+FROM musicas
+WHERE composicao NOT ILIKE '%quintet%';
