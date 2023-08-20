5d6684fc490cedbdd5bd4bc31ef1cba325ed7431
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-20 16:23:51 -0300

    inserido dados no banco de dados

--- modulo3/exercicios-backend-crud-sql/01/query.sql
+++ modulo3/exercicios-backend-crud-sql/01/query.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE exercicio_crud_sql;
 
+-------------
+
 create table usuarios (
 	id serial,
   	nome text not null,
@@ -7,3 +9,14 @@ create table usuarios (
   	email varchar(50),
   	senha varchar(8) not null
 );
+
+-------------
+
+INSERT INTO usuarios 
+(nome, idade, email, senha)
+VALUES
+('Aretha Montgomery', 30, 'augue.id.ante@odioAliquam.com', 'a0B13O3L'),
+('Camden H. Bartlett', 15, 'turpis.vitae.purus@risusDuisa.ca', 'p3P96F3Q'),
+('Raja W. Coffey', 30, 'raja.feugiat@nonummy.com', 's5F51T7L'),
+('Elton D. Olsen', 29, 'auctor@duiFuscediam.edu', 'k5X25B0R'),
+('Shelley E. Frederick', 20, 'raja.feugiat@nonummy.com', 'u2D18F6E');
