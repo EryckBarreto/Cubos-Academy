73f6eb1941c1a0f5258e0fed9116132930cae29a
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-26 18:16:57 -0300

    resolução do exercício de modelagem de dados

---
+++ modulo3/modelagem-sql/exercicios-backend-modelagem-dados/01/query.sql
@@ -0,0 +1,126 @@
+CREATE DATABASE ecommerce;
+
+CREATE TABLE categorias (
+	id serial PRIMARY KEY,
+  nome varchar(50)
+);
+
+CREATE TABLE clientes (
+	cpf char(11) UNIQUE,
+  nome varchar(150)
+);
+
+CREATE TABLE vendedores (
+	cpf char(11) UNIQUE,
+  nome varchar(150)
+);
+
+CREATE TABLE pedidos (
+	id serial PRIMARY KEY,
+  valor int,
+  cliente_cpf char(11) REFERENCES clientes(cpf),
+  vendedor_cpf char(11) REFERENCES vendedores(cpf)
+);
+
+CREATE TABLE produtos (
+	id serial PRIMARY KEY,
+	nome varchar(100),
+  descricao text,
+  preco int,
+  quantidade_em_estoque int,
+  categoria_id int REFERENCES categorias(id)
+);
+
+CREATE TABLE itens_do_pedido (
+	id serial PRIMARY KEY,
+  pedido_id int REFERENCES pedidos(id),
+  quantidade int,
+  produto_id int REFERENCES produtos(id)
+);
+
+
+INSERT INTO categorias (nome)
+VALUES
+('frutas'),
+('verduras'),
+('massas'),
+('bebidas'),
+('utilidades');
+
+INSERT INTO produtos (
+  nome, 
+  descricao, 
+  preco, 
+  quantidade_em_estoque,
+  categoria_id
+)
+VALUES
+  ('Mamão', 'Rico em vitamina A, potássio e vitamina C', 300, 123, 1),
+  ('Maça', 'Fonte de potássio e fibras.', 90, 34, 1),
+  ('Cebola', 'Rico em quercetina, antocianinas, vitaminas do complexo B, C.', 50, 76, 2),
+  ('Abacate', 'NÃO CONTÉM GLÚTEN.', 150, 64, 1),
+  ('Tomate', 'Rico em vitaminas A, B e C.', 125, 88, 2),
+  ('Acelga', 'NÃO CONTÉM GLÚTEN.', 235, 13, 2),
+  ('Macarrão parafuso', 'Sêmola de trigo enriquecida com ferro e ácido fólico, ovos e corantes naturais', 690, 5, 3),
+  ('Massa para lasanha', 'Uma reunião de família precisa ter comida boa e muita alegria.', 875, 19, 3),
+  ('Refrigerante coca cola lata', 'Sabor original', 350, 189, 4),
+  ('Refrigerante Pepsi 2l', 'NÃO CONTÉM GLÚTEN. NÃO ALCOÓLICO.', 700, 12, 4),
+  ('Cerveja Heineken 600ml', 'Heineken é uma cerveja lager Puro Malte, refrescante e de cor amarelo-dourado', 1200, 500, 4),
+  ('Agua mineral sem gás', 'Smartwater é água adicionado de sais mineirais (cálcio, potássio e magnésio) livre de sódio e com pH neutro.', 130, 478, 4),
+  ('Vassoura', 'Pigmento, matéria sintética e metal.', 2350, 30, 5),
+  ('Saco para lixo', 'Reforçado para garantir mais segurança', 1340, 90, 5),
+  ('Escova dental', 'Faça uma limpeza profunda com a tecnologia inovadora', 1000, 44, 5),
+  ('Balde para lixo 50l', 'Possui tampa e fabricado com material reciclado', 2290, 55, 5),
+  ('Manga', 'Rico em Vitamina A, potássio e vitamina C', 198, 176, 1),
+  ('Uva', 'NÃO CONTÉM GLÚTEN.', 420, 90, 1);
+  
+ 
+INSERT INTO clientes (cpf, nome)
+VALUES
+  ('80371350042', 'José Augusto Silva'),
+  ('67642869061', 'Antonio Oliveira'),
+  ('63193310034', 'Ana Rodrigues'),
+  ('75670505018', 'Maria da Conceição');
+  
+INSERT INTO vendedores (cpf, nome)
+VALUES
+  (REPLACE(REPLACE('825.398.410-31', '.', ''), '-', ''), 'Rodrigo Sampaio'),
+  (REPLACE(REPLACE('232.625.460-03', '.', ''), '-', ''), 'Beatriz Souza Santos'),
+  (REPLACE(REPLACE('280.071.550-23', '.', ''), '-', ''), 'Carlos Eduardo');
+
+INSERT INTO pedidos (valor, cliente_cpf, vendedor_cpf)
+VALUES
+(300+700+(6*1200)+1000+(90*5), '80371350042', '28007155023');
+
+INSERT INTO itens_do_pedido (pedido_id, quantidade, produto_id)
+VALUES
+(1, 1, 1),
+(1, 1, 10),
+(1, 6, 11),
+(1, 1, 16),
+(1, 5, 2);
+
+UPDATE produtos
+SET
+quantidade_em_estoque = quantidade_em_estoque - 1
+WHERE id = 1;
+
+UPDATE produtos
+SET
+quantidade_em_estoque = quantidade_em_estoque - 1
+WHERE id = 10;
+
+UPDATE produtos
+SET
+quantidade_em_estoque = quantidade_em_estoque - 6
+WHERE id = 11;
+
+UPDATE produtos
+SET
+quantidade_em_estoque = quantidade_em_estoque - 1
+WHERE id = 15;
+
+UPDATE produtos
+SET
+quantidade_em_estoque = quantidade_em_estoque - 5
+WHERE id = 2;
