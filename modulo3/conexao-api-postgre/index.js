d2f93733d44480ed9ed88e3399568d0ba1264a2f
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-28 16:18:07 -0300

    fazer conexão via pool e client

--- modulo3/conexao-api-postgre/index.js
+++ modulo3/conexao-api-postgre/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const pool = require('./conexao')
+
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json('tudo certo')
+app.get('/', async (req, res) => {
+
+    try {
+        const resultado = await pool.query('select * from empresas');
+        return res.json(resultado);
+    } catch (error) {
+        console.log(error.message);
+    };
+
 });
 
 app.listen(3000);
