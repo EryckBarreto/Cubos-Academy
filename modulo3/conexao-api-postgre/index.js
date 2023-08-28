9f90cc2692fae098ee6f315187cdf3937e44b326
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-28 18:59:18 -0300

    utilização do inner join

--- modulo3/conexao-api-postgre/index.js
+++ modulo3/conexao-api-postgre/index.js
@@ -6,18 +6,24 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/:id', async (req, res) => {
-    const { id } = req.params;
-
+app.get('/', async (req, res) => {
     try {
 
         // const query = 'SELECT * FROM empresas WHERE nome = $1 or nome = $2'
         // const params = ['Google', 'Facebook']
 
-        const query = 'UPDATE empresas SET site = $1 WHERE id = $2'
-        const params = ['www.cakewalk.com', 1];
+        // const query = 'UPDATE empresas SET site = $1 WHERE id = $2'
+        // const params = ['www.cakewalk.com', 1];
 
-        const resultado = await pool.query(query, params);
+        const query = `
+            SELECT empresas.id AS empresaId, filiais.id AS filialID, empresas.nome, filiais.pais, pessoas.nome AS funcionario 
+            FROM empresas
+            INNER JOIN filiais
+            ON empresas.id = filiais.empresa_id
+            JOIN pessoas ON pessoas.empresa_id = empresas.id;
+        `
+
+        const resultado = await pool.query(query);
 
         return res.json(resultado.rows);
     } catch (error) {
