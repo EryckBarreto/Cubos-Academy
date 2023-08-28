8d5585f243637525ce91f463e96338656feb5508
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-28 19:30:35 -0300

    paginação de registros

--- modulo3/conexao-api-postgre/index.js
+++ modulo3/conexao-api-postgre/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
+    const { pagina, porPagina } = req.query;
     try {
 
         // const query = 'SELECT * FROM empresas WHERE nome = $1 or nome = $2'
@@ -30,16 +31,29 @@ app.get('/', async (req, res) => {
         //     ON e.id = f.empresa_id;
         // `
 
+        // const query = `
+        //     SELECT e.id AS empresaId, f.id AS filialId, e.nome, f.pais 
+        //     FROM empresas e
+        //     FULL JOIN filiais f
+        //     ON e.id = f.empresa_id;
+        // `
+
         const query = `
-            SELECT e.id AS empresaId, f.id AS filialId, e.nome, f.pais 
-            FROM empresas e
-            FULL JOIN filiais f
-            ON e.id = f.empresa_id;
-        `
+            SELECT * FROM pessoas 
+            ORDER BY id ASC
+            LIMIT $1 OFFSET $2;
+            `
+        const offset = pagina === 1 ? 0 : (pagina -1) * porPagina;
 
-        const resultado = await pool.query(query);
+        const resultado = await pool.query(query, [porPagina, offset]);
+        const result = {
+            pagina,
+            porPagina,
+            total: resultado.rowCount,
+            registros: resultado.rows
+        }
 
-        return res.json(resultado.rows);
+        return res.json(result);
     } catch (error) {
         console.log(error.message);
     };
