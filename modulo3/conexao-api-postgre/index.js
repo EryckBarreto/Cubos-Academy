69b392e349e361930973eeb0696e3fc69a319cad
EryckBarreto <eryck.barreto@hotmail.com>
2023-08-28 16:47:09 -0300

    como passar valores dinâmicos nas queries de maneira segura

--- modulo3/conexao-api-postgre/index.js
+++ modulo3/conexao-api-postgre/index.js
@@ -6,11 +6,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/:id', async (req, res) => {
+    const { id } = req.params;
 
     try {
-        const resultado = await pool.query('select * from empresas');
-        return res.json(resultado);
+
+        // const query = 'SELECT * FROM empresas WHERE nome = $1 or nome = $2'
+        // const params = ['Google', 'Facebook']
+
+        const query = 'UPDATE empresas SET site = $1 WHERE id = $2'
+        const params = ['www.cakewalk.com', 1];
+
+        const resultado = await pool.query(query, params);
+
+        return res.json(resultado.rows);
     } catch (error) {
         console.log(error.message);
     };
